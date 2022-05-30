949d4b40c384513e6c3d3cbf99dbcefdf40f217f
Alex David <ad26creations@gmail.com>
2022-05-30 21:57:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ const converter = document.querySelector('.convert'); //convert button element
 
 converter.addEventListener("click", function(){
 
-let kilometres = document.querySelector(".Kmph").value; //speed in kmph
-let miles = document.querySelector(".mph").value; // speed in mph
-let metres = document.querySelector(".mps").value; //speed in metres per second
-let knot = document.querySelector(".Knots").value;    //speed in knots
+let kilometres = Number(document.querySelector(".Kmph").value); //speed in kmph
+let miles = Number(document.querySelector(".mph").value); // speed in mph
+let metres = Number(document.querySelector(".mps").value); //speed in metres per second
+let knot = Number(document.querySelector(".Knots").value);    //speed in knots
 
-if(kilometres !==null){
+if(kilometres !==0){
 
     miles = kilometres*0.621371;
     metres = kilometres*0.277778;
     knot = kilometres*0.539957;
 
+    document.querySelector(".Kmph").value = kilometres;
     document.querySelector(".mph").value = miles;
     document.querySelector(".mps").value = metres;
     document.querySelector(".Knots").value = knot;
@@ -21,13 +22,39 @@ if(kilometres !==null){
     console.log(kilometres, miles, metres, knot);
 }
 
-else if(miles !==null){
+else if(miles !==0){
 
     kilometres = miles*1.609344;
     metres = miles*0.44704;
     knot = miles*0.868976;
 
     document.querySelector(".Kmph").value = kilometres;
+    document.querySelector(".mph").value = miles;
+    document.querySelector(".mps").value = metres;
+    document.querySelector(".Knots").value = knot;
+}
+
+else if(metres !==0){
+
+kilometres = metres*3.6;
+miles = metres*2.239936;
+knot = metres*1.943844;
+
+  document.querySelector(".Kmph").value = kilometres;
+  document.querySelector(".mph").value = miles;
+  document.querySelector(".mps").value = metres;
+  document.querySelector(".Knots").value = knot;
+ 
+}
+
+else{
+
+    kilometres = knot*1.852;
+    metres = knot*0.514444;
+    miles = knot*1.150779;
+
+    document.querySelector(".Kmph").value = kilometres;
+    document.querySelector(".mph").value = miles;
     document.querySelector(".mps").value = metres;
     document.querySelector(".Knots").value = knot;
 }
