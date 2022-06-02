a86093f4977e9d41da9d5dd371023151f33a6708
Alex David <ad26creations@gmail.com>
2022-06-02 15:33:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,23 @@ let miles = Number(document.querySelector(".mph").value); // speed in mph
 let metres = Number(document.querySelector(".mps").value); //speed in metres per second
 let knot = Number(document.querySelector(".Knots").value);    //speed in knots
 
+
+const assign = function(){
+    
+    document.querySelector(".Kmph").value = kilometres;
+    document.querySelector(".mph").value = miles;
+    document.querySelector(".mps").value = metres;
+    document.querySelector(".Knots").value = knot;
+};
+
 if(kilometres !==0){
 
     miles = kilometres*0.621371;
     metres = kilometres*0.277778;
     knot = kilometres*0.539957;
 
-    document.querySelector(".Kmph").value = kilometres;
-    document.querySelector(".mph").value = miles;
-    document.querySelector(".mps").value = metres;
-    document.querySelector(".Knots").value = knot;
+    assign();
 
-
-    console.log(kilometres, miles, metres, knot);
 }
 
 else if(miles !==0){
@@ -28,10 +32,7 @@ else if(miles !==0){
     metres = miles*0.44704;
     knot = miles*0.868976;
 
-    document.querySelector(".Kmph").value = kilometres;
-    document.querySelector(".mph").value = miles;
-    document.querySelector(".mps").value = metres;
-    document.querySelector(".Knots").value = knot;
+    assign();
 }
 
 else if(metres !==0){
@@ -40,11 +41,7 @@ kilometres = metres*3.6;
 miles = metres*2.239936;
 knot = metres*1.943844;
 
-  document.querySelector(".Kmph").value = kilometres;
-  document.querySelector(".mph").value = miles;
-  document.querySelector(".mps").value = metres;
-  document.querySelector(".Knots").value = knot;
- 
+assign();
 }
 
 else{
@@ -53,10 +50,7 @@ else{
     metres = knot*0.514444;
     miles = knot*1.150779;
 
-    document.querySelector(".Kmph").value = kilometres;
-    document.querySelector(".mph").value = miles;
-    document.querySelector(".mps").value = metres;
-    document.querySelector(".Knots").value = knot;
+  assign();
 }
 
 });
